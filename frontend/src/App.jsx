434e4e1018111ff7c658e199db12c81a05aaf5e7
ronaldo <aaaaaa@gmail.com>
2025-10-18 13:36:57 +0300

    signup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,22 @@
-import { Routes, Route } from "react-router-dom";
+"use client";
+import { Routes, Route, Navigate } from "react-router-dom";
 import ChatPage from "./pages/ChatPage";
 import LoginPage from "./pages/LoginPage";
 import SignUpPage from "./pages/SignUpPage";
 import { useAuthStore } from "./store/useAuthStore";
+import { useEffect } from "react";
+import PageLoader from "./components/PageLoader";
+
+import { Toaster } from "react-hot-toast";
 
 const App = () => {
-  const { authUser, isLoggedIn, login } = useAuthStore();
+  const { authUser, isCheckingAuth, checkAuth } = useAuthStore();
 
-  console.log("auth user:", authUser);
-  console.log("isloggedin:", isLoggedIn);
+  useEffect(() => {
+    checkAuth();
+  }, [checkAuth]);
+
+  if (isCheckingAuth) return <PageLoader />;
 
   return (
     <div className="min-h-screen bg-slate-900 relative flex items-center justify-center p-4 overflow-hidden text-white">
@@ -16,15 +24,22 @@ const App = () => {
       <div className="absolute top-0 -left-4 size-96 bg-pink-500 opacity-20 blur-[100px]" />
       <div className="absolute bottom-0 -right-4 size-96 bg-cyan-500 opacity-20 blur-[100px]" />
 
-      <div className="bg-red-400 p-4 z-10">
-        <button onClick={login}>Click</button>
-      </div>
-
       <Routes>
-        <Route path="/" element={<ChatPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignUpPage />} />
+        <Route
+          path="/"
+          element={authUser ? <ChatPage /> : <Navigate to={"/login"} />}
+        />
+        <Route
+          path="/login"
+          element={!authUser ? <LoginPage /> : <Navigate to={"/"} />}
+        />
+        <Route
+          path="/signup"
+          element={!authUser ? <SignUpPage /> : <Navigate to={"/"} />}
+        />
       </Routes>
+
+      <Toaster />
     </div>
   );
 };
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const axiosObject = axios.create({
+export const axiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
       ? "http://localhost:3000/api"
